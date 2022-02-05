6b2e2e32f87a103015896c9f86907be4bf0da60e
Yeonhyuk <yhkim@autocrypt.io>
2022-02-05 23:38:26 +0900

    Initial Commit

---
+++ src/test/java/com/nfplatform/NfpBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nfplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfpBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
